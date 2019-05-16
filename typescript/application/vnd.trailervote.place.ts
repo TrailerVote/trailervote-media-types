7fcc3b3c8f3346babac6bb82e381861c08d9fa7b
Karens Grigorjancs <karens.grigorjancs@gmail.com>
2019-05-16 12:45:19 -0300

    Add typescript definitions

--- typescript/application/vnd.trailervote.place.ts
+++ typescript/application/vnd.trailervote.place.ts
@@ -1,3 +1,31 @@
+export interface PlaceBaseV3 {
+  name: string
+  parent_place: string | null
+  expires_at: string | null
+  updated_at: string
+
+  _links: {
+    self: { href: string }
+    products_archive: { href: string }
+    parent: null | { href: string }
+    children: { href: string }
+  }
+}
+
+export interface PlaceV3 {
+  place: PlaceBaseV3
+}
+
+export type PlaceCreateV3 = PlaceCreateV2
+
+export interface PlaceCollectionV3 {
+  places: {
+    _embedded: PlaceBaseV3[]
+  }
+}
+
+export type PlaceIndexV3 = PlaceIndexV2
+
 export interface PlaceBaseV2 {
   name: string
   parent_place: string | null
