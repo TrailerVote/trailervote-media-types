f17c113a62f1fdbc5f2d655ff4beac706dbb4370
Karens Grigorjancs <karens.grigorjancs@gmail.com>
2019-10-17 23:37:14 +0200

    add config definitions

--- typescript/application/vnd.trailervote.configuration.ts
+++ typescript/application/vnd.trailervote.configuration.ts
@@ -1,3 +1,37 @@
+export interface ConfigurationV4 {
+  configuration: {
+    _links: {
+      self: { href: string }
+      place: { href: string }
+      push_manifest: { href: string }
+      products: { href: string }
+      product_lookup: { href: string }
+      persona: { href: string, templated: true }
+      analytics: { href: string }
+      telemetrics: { href: string }
+      issues: { href: string }
+      persona_config: { href: string, templated: true }
+    },
+    customer_config: {}
+  }
+}
+
+export interface ConfigurationV3 {
+  configuration: {
+    _links: {
+      self: { href: string }
+      place: { href: string }
+      push_manifest: { href: string }
+      products: { href: string }
+      product_lookup: { href: string }
+      persona: { href: string, templated: true }
+      analytics: { href: string }
+      telemetrics: { href: string }
+      issues: { href: string }
+    }
+  }
+}
+
 export interface ConfigurationV2 {
   configuration: {
     _links: {
