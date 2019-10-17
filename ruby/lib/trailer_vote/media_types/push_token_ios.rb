b6d276a1f03b5722c71dce10261b495efa7b0cd7
Vincent Robbemond <vincentrobbemond@gmail.com>
2019-10-17 23:06:25 +0200

    Create PushTokenIos media type v1

---
+++ ruby/lib/trailer_vote/media_types/push_token_ios.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative 'base_text'
+require_relative 'types/iso8601'
+
+module TrailerVote
+  module MediaTypes
+    class PushTokenIos < BaseText
+      media_type 'push_token_ios', defaults: { suffix: :json, version: 1 }
+
+      validations do
+        version 1 do
+          version_1_base = ::MediaTypes::Scheme.new do
+            attribute :token, String
+            attribute :app_id, String
+            attribute :device_id, String
+          end
+
+          attribute :push_token_ios do
+            merge version_1_base
+          end
+
+          view 'create' do
+            attribute :push_token_ios do
+              merge version_1_base
+            end
+          end
+        end
+      end
+
+      registrations :push_token_ios do
+        view 'create', :create_push_token_ios
+
+        versions 1
+      end
+
+    end
+  end
+end
