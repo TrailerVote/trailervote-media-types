cc73cf5d25cdde14f55fd5bd9e74b0bc79e83f01
Vincent Robbemond <vincentrobbemond@gmail.com>
2019-10-29 15:09:57 +0100

    Add Persona v2

--- ruby/test/trailer_vote/media_types/persona_test.rb
+++ ruby/test/trailer_vote/media_types/persona_test.rb
@@ -9,14 +9,15 @@ module TrailerVote
       def test_the_default_media_type
         # When this changes, the default version has changes and you should make downstream changes / have this gems
         # version change as well. If you pin a certain media type gem version, you get consistent media types.
-        assert_equal 'application/vnd.trailervote.persona.v1+json', Persona.to_constructable.to_s
+        assert_equal 'application/vnd.trailervote.persona.v2+json', Persona.to_constructable.to_s
       end
 
       def test_it_registers
         assert_registered_types(
           Persona,
           'application/vnd.trailervote.persona.v1+json' => [:persona_v1_json, []],
-          'application/vnd.trailervote.persona.v1.create+json' => [:create_persona_v1_json, []]
+          'application/vnd.trailervote.persona.v1.create+json' => [:create_persona_v1_json, []],
+          'application/vnd.trailervote.persona.v2+json' => [:persona_v2_json, []]
         )
       end
 
