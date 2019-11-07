44b2e97b3cd906b818eef54a8a861f54b3e9b68e
Vincent Robbemond <vincentrobbemond@gmail.com>
2019-11-07 12:40:38 +0100

    Remove ordered view from audio fragment

--- ruby/lib/trailer_vote/media_types/audio_fragment.rb
+++ ruby/lib/trailer_vote/media_types/audio_fragment.rb
@@ -84,16 +84,6 @@ module TrailerVote
               not_strict
             end
           end
-
-          view 'ordered' do
-            attribute :ordered_fragments do
-              collection :_index, allow_empty: true do
-                attribute :href, Types::HttpUrl
-                attribute :q, Float
-              end
-              not_strict
-            end
-          end
         end
       end
 
@@ -108,7 +98,6 @@ module TrailerVote
       registrations :audio_fragment do
         view 'index', :audio_fragment_urls
         view 'collection', :audio_fragments
-        view 'ordered', :ordered_audio_fragment_urls
 
         versions 1
 
