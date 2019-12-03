2abd4126789910bced481b0c3d226b738d1e7759
Vincent Robbemond <vincentrobbemond@gmail.com>
2019-12-03 15:25:08 +0100

    Add Time type

---
+++ ruby/lib/trailer_vote/media_types/types/time.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module TrailerVote
+  module MediaTypes
+    module Types
+      # noinspection RubyConstantNamingConvention
+      Time = /^(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-5][0-9])$/ # rubocop:disable Naming/ConstantName, Style/MutableConstant
+    end
+  end
+end
