7a5cbc9b5023c39c4b0f2fe0bc29fdb44674c507
Vincent Robbemond <vincentrobbemond@gmail.com>
2019-12-03 15:46:55 +0100

    Actually include the seconds in regex

--- ruby/lib/trailer_vote/media_types/types/local_time.rb
+++ ruby/lib/trailer_vote/media_types/types/local_time.rb
@@ -5,7 +5,7 @@ module TrailerVote
     module Types
       # noinspection RubyConstantNamingConvention
       # |60 seconds notation needed to support leap second
-      LocalTime = /^(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-5][0-9]|60)$/ # rubocop:disable Naming/ConstantName, Style/MutableConstant
+      LocalTime = /^(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-5][0-9]):([0-5][0-9]|60)$/ # rubocop:disable Naming/ConstantName, Style/MutableConstant
     end
   end
 end
