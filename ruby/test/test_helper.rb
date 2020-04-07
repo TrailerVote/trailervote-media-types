06a9563cfa6299f77744cb8b27de7dbd94fa6369
Max Maton <info@maxmaton.nl>
2020-04-07 13:39:04 +0200

    Fix test helper

--- ruby/test/test_helper.rb
+++ ruby/test/test_helper.rb
@@ -56,13 +56,11 @@ class Minitest::Test < Minitest::Runnable
   end
 
   def assert_fixture_passes_validation(media_type)
-    ::MediaTypes.stub(:register, nil) do
-      media_type.register.flatten.each do |registerable|
-        fixture = load_fixture(registerable.media_type)
-        next unless fixture
+    media_type.available_validations.each do |v|
+      fixture = load_fixture(registerable.media_type)
+      next unless fixture
 
-        assert registerable.media_type.validate!(fixture)
-      end
+      assert v.validate!(fixture)
     end
   end
 end
